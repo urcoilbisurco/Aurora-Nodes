342dac0586295c3b2f3efaec66b126a593ffe0db
Francesco Marassi <urcoilbisurco@gmail.com>
2017-04-14 16:59:58 +0200

    add button to handle wifi error

--- switch/main.js
+++ switch/main.js
@@ -1,64 +1,55 @@
-print(process.memory());
 const env= require("./_env.js");
 const wifi = require('Wifi');
 var f = new (require("FlashEEPROM"))();
 
-function generateThanksPage(){
-  var page = `<html>
-      <div>
-        <h1>Thank you.</h1>
-        <h2>You can now close this page and restore your Wi-Fi connection.</h2>
-      </div>
-      </html>
-  `;
-  return page;
-}
-function parseRequestData(string){
-  var obj = string.split("&").reduce(function(prev, curr, i, arr) {
+//IO for Sonoff Itead
+// 13 : led
+// 0 : Button
+// 12 : Relay
+
+const parse = (string) => {
+  return string.split("&").reduce((prev, curr, i, arr)=> {
     var p = curr.split("=");
     prev[decodeURIComponent(p[0])] = decodeURIComponent(p[1]);
     return prev;
   }, {});
-  return obj;
 }
-function handleRequest(req, res) {
+const handleRequest = (req, res) => {
   console.log("connected...");
   print(process.memory());
   if (req.method=="POST") {
-    obj=parseRequestData(req.read())
+    obj=parse(req.read())
     console.log("start wifi...", obj)
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end(generateThanksPage());
-    setTimeout(function(){
+    res.writeHead(200);
+    res.end(`<html><div><h1>Thank you.</h1><h2>You can now close this page and restore your Wi-Fi connection.</h2></div></html>`);
+    setTimeout(() => {
       wifi.stopAP();
       start_wifi_and_register(obj.s, obj.p, obj.c);
       digitalWrite(13, false)
     }, 3000)
   }else{
-    wifi.scan(function(ns){
+    wifi.scan(ns => {
       print(process.memory());
-      var out=`<html><head><style>body{font-family:"Helvetica";font-size:19px;padding:64px 32px;}*{text-align:center;}</style> <meta name="viewport" content="width=device-width, initial-scale=1"></head><body>`
+      var out=`<html><style>body *{font-size:24px;padding:8px;display:block;}</style><meta name="viewport" content="width=device-width, initial-scale=1">`
       out = out+`<form method="POST" action="/"><label for="s">Choose Wifi</label><br/><select name="s" id="s">`
-      out=out+ns.map(function(n) {
-          return '<option value="'+n.ssid+'">'+n.ssid+'</option>';
-      });
+      out=out+ns.map(n => '<option value="'+n.ssid+'">'+n.ssid+'</option>');
       print(process.memory());
-      out=out+`</select><br/><label for="p">Password</label><br/><input id="p" name="p" type="password" placeholder="Password"/><br/><label for="c">Node Code</label><br/><input id="c" name="c" type="text" placeholder="1234"/><br/><input type="submit" value="save"></form>`;
+      out=out+`</select><label>Password</label><input id="p" name="p" type="text" placeholder="Password"/><label for="c">Node Code</label><input id="c" name="c" type="text" placeholder="1234"/><input type="submit" value="save"></form>`;
       console.log("connected...");
       print(process.memory());
-      res.writeHead(200, {'Content-Type':'text/html'});
-      out=out+"</body></html>"
+      out=out+"</html>"
+      res.writeHead(200);
       res.end(out);
     });
   }
 }
 
 //start AP and web server
-function onWifiError(){
+const onWifiError = () => {
   console.log("ERROR wifi")
   print(process.memory());
   wifi.setHostname("aurora")
-  wifi.startAP("aurora", {}, function(err){
+  wifi.startAP("aurora", {}, err => {
     if(err) {
         console.log("An error has occured :( ", err.message);
     } else {
@@ -69,12 +60,11 @@ function onWifiError(){
     }
   })
 }
-function check_wifi(){
-  var connect_timeout=setInterval(function(){
-    wifi.getDetails(function(obj){
+const check_wifi = () => {
+  var connect_timeout=setInterval(()=>{
+    wifi.getDetails(obj =>{
       console.log(obj.status)
       if(obj.status=="no_ap_found" || obj.status=="wrong_password" || obj.status=="off" || obj.status=="connect_failed"){
-        console.log("error, creating access point...")
         onWifiError()
         clearInterval(connect_timeout);
       }
@@ -85,18 +75,12 @@ function check_wifi(){
   },1000)
 }
 
-function register_node(code, callback){
+const register_node = code => {
   var env=require("./_env.js");
-  require("http").get(env[0]+"/api/v1/nodes/register/"+code, function(res) {
+  require("http").get(env[0]+"/api/v1/nodes/register/"+code, (res)=> {
     var c = "";
-    res.on('data', function(data) {
-      c += data;
-      console.log("data?", data)
-      console.log("JSON> ", JSON.parse(data));
-    });
-    res.on('close', function(data) {
-      console.log("?", data)
-      console.log("contents", JSON.parse(c))
+    res.on('data', (data)=> c += data)
+    res.on('close', (data)=> {
       j=JSON.parse(c)
       print(process.memory());
       f.write(3, j.user+"/"+j.uuid);
@@ -106,9 +90,9 @@ function register_node(code, callback){
     });
   });
 }
-function start_wifi_and_register(ssid, password, code){
+const start_wifi_and_register= (ssid, password, code) =>{
   check_wifi()
-  wifi.connect(ssid, { password: password }, function(error) {
+  wifi.connect(ssid, { password: password }, (error) => {
     if(error){
       onWifiError()
     }else{
@@ -121,48 +105,53 @@ function start_wifi_and_register(ssid, password, code){
     require("./blink.js")(5)
   });
 }
-function conn(callback){
+const conn=(callback) =>{
   check_wifi()
-  let ssid=E.toString(f.read(0))
-  let pass=E.toString(f.read(1))
+  let ssid=read(0)
+  let pass=read(1)
   console.log("saved ssid:", ssid)
   console.log("saved pass:", pass)
   if(ssid!=undefined){
-    wifi.connect(ssid, { password: pass }, function(e){
+    wifi.connect(ssid, { password: pass }, (e) => {
       if (e){
         onWifiError(e)
       }else{
-        let token=E.toString(f.read(3))
+        let token=read(3)//f.read(3))
         if(token){
           console.log("token", token)
           //require("./blink.js")(5)
           callback(token)
         }else{
-          register_node(E.toString(f.read(2)))
+          register_node(read(2))
         }
       }
    });
   }
 }
 
-function read(p){
-  let m=f.read(p)
-  if(m){
-    return E.toString(m)
-  }else{
-    return m;
-  }
+const read= pos => {
+  let p=f.read(pos);
+  return (p!=undefined ? E.toString(p) : undefined)
 }
 
+pinMode(0, 'input_pullup');
+const onClickBtn = event => {
+  console.log(`button pushed: ${ JSON.stringify(event) }`);
+  // const isOn = digitalRead(D4)
+  // digitalWrite(D4, !isOn)
+  //onWifiError();
+}
 
-function main(){
-  conn(function(topic){
+const main = ()=>{
+  setWatch(onClickBtn, 0, { repeat: true, edge: 'falling', debounce: 50 });
+  print(process.memory());
+  conn( topic =>{
     console.log("CONNECTED", topic);
     let mqtt = require("MQTT").create(env[1], {options:{port:env[2]}});
-    mqtt.subscribe(topic+"/update");
     mqtt.on('connected', () => {
-        console.log('Connected to mqtt!', topic+"/update");
-        require("./blink.js")(5)
+      mqtt.subscribe(topic+"/update");
+      console.log('Connected to mqtt!', topic+"/update");
+      require("./blink.js")(5)
     });
     mqtt.on("message", (to, m) => {
       console.log("message", JSON.parse(m));
@@ -173,5 +162,6 @@ function main(){
       console.log("server disconnected. TODO Retry!")
     })
     mqtt.connect()
+    print(process.memory());
   })
 }
