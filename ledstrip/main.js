0bc5571842c0f91d3ef98e0a7dec6cc29f5e0ba4
Francesco Marassi <urcoilbisurco@gmail.com>
2017-04-23 00:40:03 +0200

    add more functions for color effects

--- ledstrip/main.js
+++ ledstrip/main.js
@@ -1,31 +1,65 @@
 const env= require("./shared/_env.js");
 var esp8266 = require("ESP8266");
 pinMode(D2, "output");
-var leds=100;
+var leds=2;
+//GRB
 
 function main() {
   //colorLeds(Math.floor(Math.random() * 256), Math.floor(Math.random() * 256), Math.floor(Math.random() * 256));
-  ping(0,255,0,0)
+  setTimeout(()=>{
+    console.log("1 second")
+  },1000)
+  let colors=[0,0,0];
+  reverse_pulse(colors, 255, 0, -5);
 }
 
-function ping(led, red,green,blue){
-  var data=[];
+function reverse_pulse(colors, start,end,step){
+  console.log("colors", colors);
+  let data=[];
+  for (var j=0; j<leds; j++) {
+    data.push(...colors)
+  }
+  esp8266.neopixelWrite(NodeMCU.D2, data);
+  if(start>end){
+    setTimeout(()=>{
+      colors=[start,start,start]//, colors[1]+1, colors[2]+1];
+      pulse(colors,start-step, end, step)
+    },1)
+  }
+}
+function pulse(colors, start, end, step){
+  console.log("colors", colors);
+  let data=[];
+  for (var j=0; j<leds; j++) {
+    data.push(...colors)
+  }
+  esp8266.neopixelWrite(NodeMCU.D2, data);
+  if(start<end){
+    setTimeout(()=>{
+      colors=[start,start,start]//, colors[1]+1, colors[2]+1];
+      pulse(colors,start-step, end, step)
+    },1)
+  }
+}
+
+function ping(led, colors){
+  console.log("led", led);
+  print(process.memory());
+  let data=[];
   for (var i=0; i<leds; i++) {
     if(i==led){
-      data.push(green);
-      data.push(red);
-      data.push(blue);
+      data.push(...colors)
     }else{
-      data.push(0);
-      data.push(0);
-      data.push(0);
+      data.push(...[0,0,0]);
     }
   }
   esp8266.neopixelWrite(NodeMCU.D2, data);
+  data=null;
+  let led2=led+1;
   if(led<leds){
-    setTimeout(()=>{
-      ping(led+1, red,green,blue)
-    },1000)
+  setTimeout(()=>{
+      ping(led2, colors)
+  },1)
   }
 }
 
